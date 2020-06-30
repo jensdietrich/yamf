7f75cc8406a765c4c27624e8bf5d9356d40eaaf5
Jens Dietrich <jens.dietrich@gmail.com>
2020-07-01 11:17:32 +1200

    better reporting with attachment if OS command fails

--- yamf-core/src/main/java/nz/ac/wgtn/yamf/commons/OS.java
+++ yamf-core/src/main/java/nz/ac/wgtn/yamf/commons/OS.java
@@ -1,10 +1,14 @@
 package nz.ac.wgtn.yamf.commons;
 
+import nz.ac.wgtn.yamf.Attachment;
+import nz.ac.wgtn.yamf.Attachments;
 import org.zeroturnaround.exec.ProcessExecutor;
 import org.zeroturnaround.exec.ProcessResult;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Utilities to interact with the OS, in particular to run commands.
@@ -13,18 +17,34 @@ import java.nio.file.Paths;
 public class OS {
 
     public static ProcessResult exe(String... command) throws Exception {
-        return new ProcessExecutor()
+        ProcessResult result = new ProcessExecutor()
             .readOutput(true)
             .command(command)
             .execute();
+        recordErrorLog(result);
+        return result;
     }
 
     public static ProcessResult exe(File workingDir, String... command) throws Exception {
-        return new ProcessExecutor()
+        ProcessResult result = new ProcessExecutor()
             .readOutput(true)
             .directory(workingDir)
             .command(command)
             .execute();
+        recordErrorLog(result);
+        return result;
     }
 
+    private static void recordErrorLog(ProcessResult result) {
+        if (result.getExitValue()!=0) {
+            String output = result.outputString();
+            if (output!=null) {
+                List<String> lines = new BufferedReader(new StringReader(output))
+                    .lines()
+                    .collect(Collectors.toList());
+                Attachment attachment = new Attachment("console output",lines,"text/plain");
+                Attachments.add(attachment);
+            }
+        }
+    }
 }
