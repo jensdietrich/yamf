ad9d3721bf0b78900d649412be19f7f3878533b1
Jens Dietrich <jens.dietrich@gmail.com>
2021-04-01 11:48:13 +1300

    added junit assertion proxy that saves the console output of tests as attachment

---
+++ yamf-core/src/main/java/nz/ac/wgtn/yamf/AssertNAttach.java
@@ -0,0 +1,51 @@
+package nz.ac.wgtn.yamf;
+
+import org.junit.jupiter.api.Assertions;
+
+import javax.annotation.Nonnull;
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Improved junit5 assertions creating attachments.
+ * The typical use case to capture console output.
+ */
+public class AssertNAttach {
+
+    public static void assertSame(Object obj1,Object obj2,String message,String attachmentName,String details) throws Exception {
+        if (obj1!=obj2) {
+            Attachment attachment = new Attachment(attachmentName, lines(details), "text/plain");
+            Attachments.add(attachment);
+        }
+        org.junit.jupiter.api.Assertions.assertSame(obj1, obj2,message);
+    }
+
+    public static void assertSame(int i1,int i2,String message,String attachmentName,String details) throws Exception {
+        if (i1!=i2) {
+            Attachment attachment = new Attachment(attachmentName, lines(details), "text/plain");
+            Attachments.add(attachment);
+        }
+        org.junit.jupiter.api.Assertions.assertSame(i1, i2,message);
+    }
+
+    public static void assertTrue(boolean value,String message,String attachmentName,String details) throws Exception {
+        if (!value) {
+            Attachment attachment = new Attachment(attachmentName, lines(details), "text/plain");
+            Attachments.add(attachment);
+        }
+        org.junit.jupiter.api.Assertions.assertTrue(value,message);
+    }
+
+    private static List<String> lines(String message) throws Exception {
+        if (message==null) {
+            return Collections.EMPTY_LIST;
+        }
+        else {
+            String[] lines = message.split("\\R");
+            return Stream.of(lines).collect(Collectors.toList());
+        }
+    }
+}
